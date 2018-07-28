74aa7b494f3125fb810bf4ab77ba46584d25507f
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-07-28 17:40:53 +0100

    Support linux with exports file

--- .exports.sh
+++ .exports.sh
@@ -1,14 +1,47 @@
 export SSH_KEY_PATH="~/.ssh/rsa_id"
-export DEFAULT_USER="joshburgess"
 
-export HOME="/Users/joshburgess"
+if [[ "$PLATFORM" == "mac" ]]; then
+    export DEFAULT_USER="joshburgess"
+    export HOME="/Users/joshburgess"
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
+    export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
 
-#
-# SET UP RBENV
-##
-export PATH=/Users/joshburgess/.rbenv/shims:/Users/joshburgess//.rbenv/completions/rbenv.bash:$PATH
+    ##
+    # SET UP RBENV
+    ##
+    export PATH=/Users/joshburgess/.rbenv/shims:/Users/joshburgess//.rbenv/completions/rbenv.bash:$PATH
+
+    ##
+    # SET UP BINARY FILES
+    ##
+    export PATH=/usr/bin:/bin:$PATH
+    export PATH=:/Users/joshburgess/bin:$PATH
+    export PATH=/opt/local/bin:/opt/local/sbin:Users/joshburgess/bin:$PATH
+    export PATH=/usr/local/bin:$PATH
+    export PATH="/usr/local/sbin:$PATH"
+    export PATH=/Users/joshburgess/.local/bin:$PATH
+
+    export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
+else
+    export DEFAULT_USER="crow"
+    export HOME="/home/crow"
+
+    ##
+    # SET UP RBENV
+    ##
+    export PATH=/home/crow/.rbenv/shims:/home/crow//.rbenv/completions/rbenv.bash:$PATH
+
+    ##
+    # SET UP BINARY FILES
+    ##
+    export PATH=/usr/bin:/bin:$PATH
+    export PATH=:/home/crow/bin:$PATH
+    export PATH=/opt/local/bin:/opt/local/sbin:/home/crow/bin:$PATH
+    export PATH=/usr/local/bin:$PATH
+    export PATH="/usr/local/sbin:$PATH"
+    export PATH=/home/crow/.local/bin:$PATH
+fi;
+
 
 ##
 # SET UP PYENV
@@ -20,12 +53,4 @@ if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
 fi
 
-##
-# SET UP BINARY FILES
-##
-export PATH=/usr/bin:/bin:$PATH
-export PATH=:/Users/joshburgess/bin:$PATH
-export PATH=/opt/local/bin:/opt/local/sbin:Users/joshburgess/bin:$PATH
-export PATH=/usr/local/bin:$PATH
-export PATH="/usr/local/sbin:$PATH"
-export PATH=/Users/joshburgess/.local/bin:$PATH
+source $HOME/.cargo/env
