2426892a2ce853aa4df2157b46b21e60e6c481e0
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-11-20 20:06:31 +0000

    replace i3blocks with polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,9 @@
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload bottom &
+fi
