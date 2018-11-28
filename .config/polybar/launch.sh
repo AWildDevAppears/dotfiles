e63c7a296cde3dfc24cf288ceca074ca815a67f3
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-11-28 15:35:32 +0000

    Start changing up how we display our polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,8 +2,12 @@ killall -q polybar
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bottom &
+    MONITOR=$m polybar --reload left &
+    MONITOR=$m polybar --reload right &
+    MONITOR=$m polybar --reload center &
   done
 else
-  polybar --reload bottom &
+  polybar --reload left &
+  polybar --reload right &
+  polybar --reload center &
 fi
