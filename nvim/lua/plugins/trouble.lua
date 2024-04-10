d2cb6e905ecf7bfc17557c68e278acce41e51880
AWildDevAppears <joshuacburgess32@gmail.com>
2024-04-10 08:09:54 +0100

    Fix trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -4,44 +4,14 @@ return {
         "nvim-tree/nvim-web-devicons",
         "folke/which-key.nvim"
     },
-    lazy = true,
     opts = {
     },
-    keys = {
-        {
-            "<leader>tt",
-            function()
-                require("trouble").toggle()
-            end,
-            mode = { "n" },
-        },
-        {
-            "<leader>tw",
-            function()
-                require("trouble").toggle("workspace_diagnostics")
-            end,
-            mode = { "n" },
-        },
-        {
-            "<leader>td",
-            function()
-                require("trouble").toggle("document_diagnostics")
-            end,
-            mode = { "n" },
-        },
-        {
-            "<leader>tq",
-            function()
-                require("trouble").toggle("quickfix")
-            end,
-            mode = { "n" },
-        },
-        {
-            "<leader>tl",
-            function()
-                require("trouble").toggle("loclist")
-            end,
-            mode = { "n" },
-        },
-    },
+    init = function()
+        vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end)
+        vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end)
+        vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end)
+        vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end)
+        vim.keymap.set("n", "<leader>tl", function() require("trouble").toggle("loclist") end)
+        vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+    end,
 }
