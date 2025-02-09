92f1d239b0dd1ea34e998c64f1d4941cb49bc3e9
Josh Burgess <joshuacburgess32@gmail.com>
2025-02-09 22:42:59 +0000

    Try and get prettier to behave

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,28 +1,69 @@
+local configs_location = vim.fn.stdpath("config") .. "/configs/"
+
+function has_local_config(location, config_names)
+    local has = false
+    for i in pairs(config_names) do
+        local found = vim.fs.find(config_names[i], { upward = true, location })
+        if not (next(found) == nil) then
+            has = true
+            break
+        end
+    end
+    return has
+end
+
 return {
     "stevearc/conform.nvim",
-    opts = {
-        format = {
-            timeout_ms = 3000,
-            async = false,
-            quiet = false,
-        },
-        -- format_on_save = {
-        --     timeout_ms = 500,
-        --     lsp_fallback = true,
-        -- },
-        formattters_by_ft = {
-            ["*"] = { "codespell", "trim_whitespace" },
-            lua = { "stylua" },
-            javascript = { "prettier" },
-            typescript = { "prettier" },
-            javascriptreact = { "prettier" },
-            typescriptreact = { "prettier" },
-            vue = { "prettier" },
-            html = { "prettier" },
-            css = { "prettier" },
-            golang = { "gofumpt", "goimports", "golines" },
-        }
-    },
+    import = "plugins.formatters",
+    config = function(_, opts)
+        local formatters = {}
+        for formatter, formatter_settings in pairs(opts) do
+            formatter = formatter:gsub("_", "-")
+            formatters[formatter] = {
+                prepend_args = function(_, ctx)
+                    if has_local_config(ctx.filename, formatter_settings.config_names) then
+                        return {}
+                    else
+                        if formatter_settings.continuous_string then
+                            return {
+                                formatter_settings.config_command .. configs_location .. formatter_settings.config_path,
+                            }
+                        else
+                            return {
+                                formatter_settings.config_command,
+                                configs_location .. formatter_settings.config_path,
+                            }
+                        end
+                    end
+                end,
+            }
+        end
+
+        require("conform").setup({
+            format = {
+                timeout_ms = 3000,
+                async = false,
+                quiet = false,
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+            formattters_by_ft = {
+                ["*"] = { "codespell", "trim_whitespace" },
+                lua = { "stylua" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                vue = { "prettier" },
+                html = { "prettier" },
+                css = { "prettier" },
+                golang = { "gofumpt", "goimports", "golines" },
+            },
+            formatters = formatters,
+        })
+    end,
     dependencies = { "mason.nvim" },
     lazy = true,
     cmd = { "ConformInfo" },
@@ -35,10 +76,10 @@ return {
         })
 
         vim.api.nvim_create_autocmd("BufWritePre", {
-          pattern = "*",
-          callback = function(args)
-            require("conform").format({ bufnr = args.buf })
-          end,
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
         })
 
         vim.api.nvim_create_user_command("Format", function(args)
