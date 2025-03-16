c4179ca6b5dbc8a0624382153b2ac226ce10c02a
Josh Burgess <joshuacburgess32@gmail.com>
2025-03-16 19:23:50 +0000

    Fix formatters

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -14,89 +14,28 @@ end
 
 return {
     "stevearc/conform.nvim",
-    import = "plugins.formatters",
-    config = function(_, opts)
-        local formatters = {}
-        for formatter, formatter_settings in pairs(opts) do
-            formatter = formatter:gsub("_", "-")
-            formatters[formatter] = {
-                prepend_args = function(_, ctx)
-                    if has_local_config(ctx.filename, formatter_settings.config_names) then
-                        return {}
-                    else
-                        if formatter_settings.continuous_string then
-                            return {
-                                formatter_settings.config_command .. configs_location .. formatter_settings.config_path,
-                            }
-                        else
-                            return {
-                                formatter_settings.config_command,
-                                configs_location .. formatter_settings.config_path,
-                            }
-                        end
-                    end
-                end,
-            }
-        end
-
-        require("conform").setup({
-            format = {
-                timeout_ms = 3000,
-                async = false,
-                quiet = false,
-            },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
-            formattters_by_ft = {
-                ["*"] = { "codespell", "trim_whitespace" },
-                lua = { "stylua" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                vue = { "prettier" },
-                html = { "prettier" },
-                css = { "prettier" },
-                golang = { "gofumpt", "goimports", "golines" },
-            },
-            formatters = formatters,
-        })
-    end,
+    opts = {
+        format = {
+            timeout_ms = 3000,
+            async = false,
+            quiet = false,
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true,
+        },
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            vue = { "prettier" },
+            html = { "prettier" },
+            css = { "prettier" },
+            golang = { "gofumpt", "goimports", "golines" },
+            ["*"] = { "codespell", "trim_whitespace" },
+        },
+    },
     dependencies = { "mason.nvim" },
-    lazy = true,
-    cmd = { "ConformInfo" },
-    init = function()
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-
-        vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-            pattern = "*",
-            command = [[%s/\s\+$//e]],
-        })
-
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args)
-                require("conform").format({ bufnr = args.buf })
-            end,
-        })
-
-        vim.api.nvim_create_user_command("Format", function(args)
-            local range = nil
-            if args.count ~= -1 then
-                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-                range = {
-                    start = { args.line1, 0 },
-                    ["end"] = { args.line2, end_line:len() },
-                }
-            end
-            require("conform").format({ async = true, lsp_fallback = true, range = range })
-        end, { range = true })
-
-        local binds = require("keybinds")
-
-        vim.keymap.set("n", binds.file_format, "<cmd>Format<cr>", {})
-        vim.keymap.set("n", binds.macro_file_format, "<cmd>Format<cr>", {})
-    end
 }
