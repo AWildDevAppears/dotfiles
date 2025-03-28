e89e0edc9b831ac69008cf428c696cc5f34cc0a2
Josh Burgess <joshuacburgess32@gmail.com>
2025-03-28 10:03:37 +0000

    decrufting

--- nvim/lua/plugins/actions.lua
+++ nvim/lua/plugins/actions.lua
@@ -6,48 +6,48 @@ return {
         local binds = require("keybinds")
         vim.keymap.set({ "v", "n" }, binds.code_actions, require("actions-preview").code_actions)
     end,
-    keys = {
-        {
-            binds.hover_docs,
-            function()
-                vim.lsp.buf.signature_help()
-            end
-        },
-        {
-            binds.rename,
-            function()
-                vim.lsp.buf.rename()
-            end
-        },
-        {
-            binds.diag_next,
-            function()
-                vim.diagnostic.goto_next()
-            end
-        },
-        {
-            binds.diag_prev,
-            function()
-                vim.diagnostic.goto_prev()
-            end
-        },
-        {
-            binds.goto_references,
-            function()
-                vim.lsp.buf.references()
-            end
-        },
-        {
-            binds.goto_definition,
-            function()
-                vim.lsp.buf.definition()
-            end
-        },
-        {
-            binds.goto_type_definition,
-            function()
-                vim.lsp.buf.type_definition()
-            end
-        },
-    }
+    -- keys = {
+    --     {
+    --         binds.hover_docs,
+    --         function()
+    --             vim.lsp.buf.signature_help()
+    --         end
+    --     },
+    --     {
+    --         binds.rename,
+    --         function()
+    --             vim.lsp.buf.rename()
+    --         end
+    --     },
+    --     {
+    --         binds.diag_next,
+    --         function()
+    --             vim.diagnostic.goto_next()
+    --         end
+    --     },
+    --     {
+    --         binds.diag_prev,
+    --         function()
+    --             vim.diagnostic.goto_prev()
+    --         end
+    --     },
+    --     {
+    --         binds.goto_references,
+    --         function()
+    --             vim.lsp.buf.references()
+    --         end
+    --     },
+    --     {
+    --         binds.goto_definition,
+    --         function()
+    --             vim.lsp.buf.definition()
+    --         end
+    --     },
+    --     {
+    --         binds.goto_type_definition,
+    --         function()
+    --             vim.lsp.buf.type_definition()
+    --         end
+    --     },
+    -- }
 }
--- nvim/lua/plugins/find.lua
+++ nvim/lua/plugins/find.lua
@@ -8,6 +8,14 @@ return {
     dependencies = {
         "neovim/nvim-lspconfig",
     },
+    -- @type snacks.Config
+    opts = {
+        statuscolumn = {
+            left = { "mark", "sign" },
+            right = { "fold", "git" },
+            enabled = true,
+        },
+    },
     keys = {
         { binds.find_file,
             function()
@@ -31,6 +39,54 @@ return {
                 Snacks.picker.diagnostics_buffer()
             end
         },
+        {
+            binds.hover_docs,
+            function()
+                vim.lsp.buf.signature_help()
+            end
+        },
+        {
+            binds.rename,
+            function()
+                vim.lsp.buf.rename()
+            end
+        },
+        {
+            binds.diag_next,
+            function()
+                vim.diagnostic.goto_next()
+            end
+        },
+        {
+            binds.diag_prev,
+            function()
+                vim.diagnostic.goto_prev()
+            end
+        },
+        {
+            binds.goto_references,
+            function()
+                vim.lsp.buf.references()
+            end
+        },
+        {
+            binds.goto_definition,
+            function()
+                vim.lsp.buf.definition()
+            end
+        },
+        {
+            binds.goto_type_definition,
+            function()
+                vim.lsp.buf.type_definition()
+            end
+        },
+        {
+            binds.toggle_term,
+            function ()
+                Snacks.terminal.toggle()
+            end
+        }
     },
 
 }
